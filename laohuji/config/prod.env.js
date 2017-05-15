58b01455c8819afb70ea333a7b0e9a394cd1b432
hsp130208358005 <13020835005@163.com>
2017-05-15 20:41:37 +0800

    config

--- laohuji/config/prod.env.js
+++ laohuji/config/prod.env.js
@@ -6,7 +6,7 @@ module.exports = {
 	index: path.resolve(__dirname, '../dist/index.html'),
 	assetsRoot: path.resolve(__dirname, '../dist'),
 	assetsSubDirectory: 'static',
-	assetsPublicPath: 'http://shanlingame.oneforce.cn/sljractivite/',
+	assetsPublicPath: 'http://shanlingame.oneforce.cn/laohuji/',
 	productionSourceMap: true,
 	// Gzip off by default as many popular static hosts such as
 	// Surge or Netlify already gzip all static assets for you.
