fc09bce6e1af3ad8bd216f0cb1eb5a16a0bf5b82
18217071831 <18217071831@163.com>
2017-05-25 19:06:02 +0800

    123

--- activity-61/src/main.js
+++ activity-61/src/main.js
@@ -84,6 +84,7 @@ window.onload = function(){
 }
 /*提供一个全局方法，用来开启摄像头*/
 Vue.prototype.$getPhoto = function(){
+    alert("调用摄像头开始")
     wx.chooseImage({
        count: 1, // 默认9
        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
