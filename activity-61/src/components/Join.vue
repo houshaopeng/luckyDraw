3f11815fc5ee17894979e34a3052ccb800ee8a8e
18217071831 <18217071831@163.com>
2017-05-25 15:24:04 +0800

    报名页面

--- activity-61/src/components/Join.vue
+++ activity-61/src/components/Join.vue
@@ -1,33 +1,16 @@
 <template>
   <div class="join">
-    <div class="topLine">
-      <h3>家长信息</h3>
-    </div>
-    <div class="nickName">
-      <span>主人</span>
-      <span>ding</span>
-      <span class="headImg"><!--<img src="#" alt=""/>--></span>
-    </div>
-    <div class="telNum">
-      <span>手机号</span>
-      <input type="number" placeholder="请输入手机号" v-model="telNum" @keydown="setLength(11)"/>
-    </div>
-    <div class="info">
-      <h3>报名信息</h3>
-    </div>
-    <div class="infoitem">
-      <textarea name="" id="" cols="20" rows="2" placeholder="请输入参赛宣言，15-30个字"></textarea>
-      <div class="camera" @click='getPhoto'>
-        <img src="../assets/photo.jpg" alt=""/>
-      </div>
-      <!-- <p>请上传一张照片<input type="file"/></p> -->
-      <p>请上传一张照片</p>
-    </div>
-    <div class="bottom">
-      <div class="sign">提交</div>
-      <p class="p1">提交报名后，您的作品将进入审核系统</p>
-      <p class="p2">请留意【作品】随时获取审核信息</p>
-    </div>
+    <span class="joinNum">1000</span>
+    <span class="tikNum">1000</span>
+    <span class="openNUm">1000</span>
+
+    <div class="Photo"></div>
+
+    <input type="text" name="" class="name">
+    <input type="number" name="" class="tel"></input>
+
+    <button class="tijiao"></button>
+    <button class="guize"></button>
   </div>
 </template>
 
@@ -60,162 +43,81 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .join{
-    background-color: #f4f4f4
-  }
-  .topLine{
-    width: 100%;
-    height: 1.2rem;
-    overflow: hidden;
-  }
-  .topLine h3{
-    height: 0.32rem;
-    margin-top: 0.6rem;
-    margin-left: 0.47rem;
-    color: #999999;
-  }
-  .nickName{
-    width: 100%;
-    height: 1.44rem;
-    background-color: white;
-    position: relative;
-  }
-  .nickName span:nth-child(1){
-    font-size: 0.4rem;
-    position: absolute;
-    top: 50%;
-    transform: translate(0,-50%);
-    left: 0.6rem;
-  }
-  .nickName span:nth-child(2){
-    font-size: 0.35rem;
-    position: absolute;
-    top: 50%;
-    transform: translate(0,-50%);
-    right: 1.8rem;
-    color: #9999a5;
-  }
-  .nickName .headImg{
-    position: absolute;
-    top: 50%;
-    transform: translate(0,-50%);
-    right: 0.53rem;
-    background-color: chocolate;
-    width: 0.9rem;
-    height: 0.9rem;
-    border-radius: 0.45rem;
-  }
-  .telNum{
-    width: 100%;
-    height: 1.44rem;
-    background-color: white;
-    position: relative;
-    margin-top: 2px;
-  }
-  .telNum span:nth-child(1){
-    font-size: 0.4rem;
-    position: absolute;
-    top: 50%;
-    transform: translate(0,-50%);
-    left: 0.6rem;
-  }
-  .telNum input{
-    position: absolute;
-    top: 50%;
-    transform: translate(0,-50%);
-    right: 0.55rem;
-    height: 1rem;
-    line-height: 1rem;
-    border: none;
-    font-size: 0.43rem;
-    width: 2.6rem;
-    color: #cccfda;
-  }
-  .info{
-    width: 100%;
-    height: 1.2rem;
-    position: relative;
-  }
-  .info h3{
-    font-size: 0.32rem;
-    color: #999999;
-    position: absolute;
-    top: 50%;
-    transform: translate(0,-50%);
-    left: 0.53rem;
-  }
-  .infoitem{
-    position: relative;
-    width: 100%;
-    height: 6.06rem;
-    background-color: white;
-  }
-  .infoitem textarea{
-    width: 8.4rem;
-    height: 2.3rem;
-    position: absolute;
-    top: 0.46rem;
-    left: 0.46rem;
-    font-size: 0.37rem;
-    border: none;
-  }
-  .infoitem .camera{
-    position: absolute;
-    width: 2.46rem;
-    height: 2.46rem;
-    border: 1px dashed gray;
-    border-radius: 10px;
-    top: 2.83rem;
-    left: 0.4rem;
-  }
-  .camera img{
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%,-50%);
-    width: 0.91rem;
-    height: 0.8rem;
-  }
-  .infoitem p{
-    position: absolute;
-    bottom: 0;
-    height: 0.86rem;
-    line-height: 0.86rem;
-    left: 0.46rem;
-  }
-  .bottom{
-    width: 100%;
-    height: 5.8rem;
-    position: relative;
-  }
-  .bottom .sign{
-    width: 8.53rem;
-    height: 1.2rem;
-    position: absolute;
-    background-color: #f2473d;
-    top: 0.53rem;
-    left: 50%;
-    transform: translate(-50%,0);
-    border-radius: 0.24rem;
-    font-size: 0.4rem;
-    text-align: center;
-    line-height: 1.2rem;
-    color: white;
-  }
-  .bottom .p1{
-    width: 6.1rem;
-    font-size: 0.35rem;
-    position: absolute;
-    top: 4.6rem;
-    left: 50%;
-    transform: translate(-50%,0);
-  }
-  .bottom .p2{
-    width: 5.3rem;
-    font-size: 0.35rem;
-    position: absolute;
-    top: 5rem;
-    left: 50%;
-    transform: translate(-50%,0);
-  }
+/* 最外层 */
+.join{
+  width:100%;
+  height:17.64rem;
+  background-image: url('../assets/join_bg.jpg');
+  background-size:100%;
+  position:relative;
+}
+.join>span{
+  width:1.87rem;
+  height:0.67rem;
+  position:absolute;
+  top: 1.6rem;
+  font-size:0.32rem;
+  text-align:center;
+  color: #008fe5;
+  line-height:0.67rem;
+}
+.joinNum{
+  left: 1.18rem;
+}
+.tikNum{
+  left: 4.2rem;
+}
+.openNUm{
+  left: 7.35rem;
+}
+
+/* 图片预览 */
+.Photo{
+  width:2.33rem;
+  height:2.33rem;
+  background-size:100%;
+  background-image:url('../assets/join_getPhoto.png');
+  position:absolute;
+  top: 4rem;
+  left: 2.67rem;
+}
+/* input框 */
+.join input{
+  width:5.73rem;
+  height:0.49rem;
+  border-radius:0.24rem;
+  border:none;
+  position:absolute;
+  left:2.8rem;
+}
+.name{
+  top:6.93rem;
+}
+.tel{
+  top:7.87rem;
+}
+.tijiao{
+  width:3.13rem;
+  height:0.85rem;
+  position:absolute;
+  background-color:transparent;
+  background-image: url('../assets/join_tijiao.png');
+  background-size:100%;
+  border:none;
+  top:9.26rem;
+  left: 50%;
+  transform:translate(-50%,0);
+}
+.guize{
+  width:3.13rem;
+  height:0.85rem;
+  position:absolute;
+  background-color:transparent;
+  background-image: url('../assets/join_guize.png');
+  background-size:100%;
+  border:none;
+  top:12.5rem;
+  left: 50%;
+  transform:translate(-50%,0);
+}
 </style>
