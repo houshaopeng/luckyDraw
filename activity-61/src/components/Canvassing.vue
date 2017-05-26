03156b1d37a818c3d7912354c40b71df02e6abd7
18217071831 <18217071831@163.com>
2017-05-26 23:22:08 +0800

    第四次提交：修改了拉票页面的样式

--- activity-61/src/components/Canvassing.vue
+++ activity-61/src/components/Canvassing.vue
@@ -91,6 +91,10 @@ export default {
       top:  pxTorem(190px);
       left: 50%;
     }
+    .canvassing_pic img{
+      width:100%;
+      height:100%;
+    }
     .btn_box{
       position: absolute;
       left: 50%;
